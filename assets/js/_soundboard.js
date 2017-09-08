88ce6fc4870505df44d678aac4ceb02aa510f228
deeuu <deeuu@users.noreply.github.com>
2017-09-08 16:49:13 +0100

    Fixed soundboard

--- assets/js/_soundboard.js
+++ assets/js/_soundboard.js
@@ -2,36 +2,36 @@ function Soundboard(config) {
 
     // Grab audio urls relative to site url
     urls = [];
-    ids = [];
-    for (var i = 0; i < config.sets.length; ++i)
+    for (var i = 0; i < config.rows.length; ++i)
     {
-        for (var j = 0; j < config.sets[i].sounds.length; ++j)
+        for (var j = 0; j < config.rows[i].sounds.length; ++j)
         {
-            var thisSound = config.sets[i].sounds[j];
+            var thisSound = config.rows[i].sounds[j];
             urls.push(config.siteURL + '/' + thisSound.url);
-            ids.push(thisSound.id);
         }
     }
 
     // Configure the audio loader
-    this.loader = new AudioLoader(urls, null);
-    this.loader.continuousPlayback = config.continuous_playback;
-    this.loader.loopPlayback = config.loop_playback;
+    this.loader = new AudioLoader(urls,
+                                  config.continuous_playback,
+                                  config.loop_playback)
     this.loader.load();
 
-    // Map buttons to audio
-    for (var i = 0; i < ids.length; ++i)
-    {
-        var btn = document.getElementById('btn-' + ids[i]);
+    // Configure play buttons:
 
-        (function (i, loader) {
-            btn.addEventListener("click", function() {
-                loader.play(i);
-            });
-        })(i, this.loader);
-    }
+    var play = function (i) {
+        this.loader.play (i);
+    }.bind(this);
 
-    // Stop audio
-    var btn = document.getElementById('soundboard-stop');
-    btn.addEventListener("click", this.loader.stop.bind(this.loader));
+    $activePage (".soundboard-play").each (function (i) {
+
+        $(this).off().on ('click', function(i){
+
+            play (i);
+
+        }.bind (null, i));
+
+    });
+
+    $activePage (".soundboard-stop").off().on ('click', this.loader.stop.bind(this.loader));
 }
